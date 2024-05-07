e28debbca83355065d77a5d5414779d646cddce6
Igor Gomes Leandro <igorgleandro@icloud.com>
2024-05-07 12:12:45 +0100

    Finished Sync Folder in Python

--- noCommentsCode.py
+++ noCommentsCode.py
@@ -2,28 +2,12 @@ import os
 import shutil
 import time
 
-
-
-# - Log file creation
-
 logFile = '/Users/igorgleandro/Desktop/Escritorio_Igor_iMac/myRepoGithub/TestTask_SyncFiles/src/log/sync_log.txt' #DEFINE DESTINATION FOR YOUR LOGFILE HERE
-
-
-# In this section:
-# - I'm defining the interval time duration (1 minute) between syncs.
-# - I start the 'while true' loop to run indefinitely.
-# - The Sleep function is invoked at the end of each loop.
-# - Added a Try Catch case error.
 
 timerDuration = 60
 
 while True:
     try:
-
-        # In this block:
-        # - Synchronization is initiated, and a log is created for each loop.
-        # - From now on, every action will generate a log.
-
         os.chdir('/Users/igorgleandro/Desktop/Escritorio_Igor_iMac/myRepoGithub/TestTask_SyncFiles/src/input') #DEFINE THE SOURCE TO BE SYNC HERE
         pathOrigin ='/Users/igorgleandro/Desktop/Escritorio_Igor_iMac/myRepoGithub/TestTask_SyncFiles/src/input' #DEFINE THE SOURCE TO BE SYNC HERE
         pathDestination = '/Users/igorgleandro/Desktop/Escritorio_Igor_iMac/myRepoGithub/TestTask_SyncFiles/src/output'#DEFINE THE SYNCED FOLDER.
@@ -31,19 +15,9 @@ while True:
         inputFilesList = os.listdir()
         outputFilesList = os.listdir(pathDestination)
 
-
-        # In this block:
-        # - Synchronization is started, and a log is created for each loop.
-        # - From now on, for every action will be add the function to generate a log.
-
         print("\n" + "Sync started" + "\n")
         with open(logFile, 'a') as f:
             f.write("\nSync started at: " + time.strftime("%Y-%m-%d %H:%M:%S") + "\n")
-
-
-        # In this section_
-        # - I´m creating an Walk through for each subfolder to be synced (input).
-        # - after checking if this folder Exist in the destination(output), if doesn't exist, creat a new folder
 
         for root, dirs, files in os.walk(pathOrigin):
             relativeInputPath = os.path.relpath(root, pathOrigin)
@@ -51,10 +25,6 @@ while True:
 
             if not os.path.exists(destinationFolder):
                 os.makedirs(destinationFolder)
-
-        
-        # In this code block:
-        # - For each file in the input folder and subfolder, a copy is made at the destination path.
 
             for file in files: 
                 sourceFile = os.path.join(root, file)
@@ -64,15 +34,6 @@ while True:
         
             with open(logFile, 'a') as f:
                 f.write("Copied: " + os.path.join(os.getcwd(), file) + " to " + os.path.join(pathDestination, file) + "\n")
-
-        
-
-        # In this code block:
-        # - A comparison between the lists is performed.
-        # - If a file doesn't exist in the output folder, it is removed.
-        # - The same logic is applied for the directory, if the Dir doesn't exist in the output folder, it is removed.
-        # - The input and output lists are then updated.
-
 
         for root, dirs, files in os.walk(pathDestination):
             for file in files:
@@ -107,15 +68,9 @@ while True:
         inputFilesList = os.listdir() 
         outputFilesList = os.listdir(pathDestination)
 
-        # - List files in the destination folder path after copying, creating, or removing.
-
         print("\n" + "Files in the output folder path:")
         for file in os.listdir(pathDestination):
             print("file name: " + file)
-
-        # In this block:
-        # - End of Sync message.
-        # - Added to the Log File   
 
         print("\n" + "Synchronizaion done.")
         
@@ -126,5 +81,3 @@ while True:
         print("An error occurred:", str(e))
 
     time.sleep(timerDuration)
-    
-
